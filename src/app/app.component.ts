c1a74033557ec706088268d432a623656a6b6ee3
AndroGenium <chilachava.doc@gmail.com>
2024-05-13 18:21:20 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BindingHw';
+
+  products = [
+    {
+      name: `Lightning McQueen Light Up Crocs Men’s Size`,
+      price: 99.99,
+      left_in_storage: 1,
+      image: `https://i.ebayimg.com/images/g/6zAAAOSwTnllIAMu/s-l1600.webp`,
+      description: `trusted retailer, lightning fast delivery, no delivery damage.`
+    },
+    {
+      name: `Lightning McQueen Light Up Crocs Men’s Size`,
+      price: 99.99,
+      left_in_storage: 1,
+      image: `https://i.ebayimg.com/images/g/6zAAAOSwTnllIAMu/s-l1600.webp`,
+      description: `trusted retailer, lightning fast delivery, no delivery damage.`
+    },
+    {
+      name: `Lightning McQueen Light Up Crocs Men’s Size`,
+      price: 99.99,
+      left_in_storage: 1,
+      image: `https://i.ebayimg.com/images/g/6zAAAOSwTnllIAMu/s-l1600.webp`,
+      description: `trusted retailer, lightning fast delivery, no delivery damage.`
+    },
+    {
+      name: `Lightning McQueen Light Up Crocs Men’s Size`,
+      price: 99.99,
+      left_in_storage: 1,
+      image: `https://i.ebayimg.com/images/g/6zAAAOSwTnllIAMu/s-l1600.webp`,
+      description: `trusted retailer, lightning fast delivery, no delivery damage.`
+    }
+    //vitom apidan vadzrob
+  ]
+
+
 }
